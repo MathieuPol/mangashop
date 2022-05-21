8331e3b320e37a9bd35931c99988123cc8b00e52
MathieuPol <poltorakmathieu@gmail.com>
2022-05-21 20:38:01 +0200

    SPA

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,31 @@
 const usingApi = {
+
+    init: function(){
+        document.querySelector('#list').addEventListener("click", usingApi.handleList);
+        document.querySelector('#home').addEventListener("click", usingApi.handleHome);
+    },
+
+    handleList: function(evt){
+        //Ici je charge ma seconde page au click sur le lien
+        document.querySelector('.home--content').classList.add('d-none');
+        document.querySelector('.list--content').classList.remove('d-none');
+        //ici je change le active sur les liens
+        document.querySelector('#home').classList.remove('active');
+        document.querySelector('#list').classList.add('active');
+        //j'appelle mon api pour le chargement des données
+        usingApi.loadTaskFromApi();
+
+    },
+
+    handleHome: function(evt){
+        //Ici je charge ma seconde page au click sur le lien
+        document.querySelector('.list--content').classList.add('d-none');
+        document.querySelector('.home--content').classList.remove('d-none');
+        //ici je change le active sur les liens
+        document.querySelector('#list').classList.remove('active');
+        document.querySelector('#home').classList.add('active');
+    },
+
     loadTaskFromApi: function(){
         const fetchOptions = {
             method: 'GET',
@@ -17,11 +44,12 @@ const usingApi = {
             function(jsonResponse)
             {
                 let data = jsonResponse['data'];
-                console.log(data);
-                console.log(data[7]['attributes']['coverImage']['tiny']);
 
-                const body = document.querySelector('.test');
 
+                const body = document.querySelector('.list--content .api');
+                //Je vide l'endroit où vont etre chargées les données
+                body.innerHTML = "";
+                //Je charge les données
                 for (let index = 0; index < data.length; index++) {
 
                     const cardTpl = document.getElementById('cardTpl').content.cloneNode(true);
@@ -54,5 +82,5 @@ const usingApi = {
 
    }
 }
-
-document.addEventListener('DOMContentLoaded', usingApi.loadTaskFromApi());
+document.addEventListener('DOMContentLoaded', usingApi.init());
+//document.addEventListener('DOMContentLoaded', usingApi.loadTaskFromApi());
